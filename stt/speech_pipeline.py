d550da72a6a658b9d27214ec20e3775fecb37316
AbbysoW <moldovanandrew0@gmail.com>
2026-03-15 13:48:26 +0300

    deleted stt activation function

--- stt/speech_pipeline.py
+++ stt/speech_pipeline.py
@@ -9,45 +9,23 @@ class Pipeline:
 
     def __init__(self):
         pass
-
-    def _is_AGAT_called(self, audio):
-        text = self.small.get_text(audio)['text']
-
-        AGAT_name = ['agat', 'агат']
-
-        # print("text:", text)
-
-        if any(name in text.lower() for name in AGAT_name) or True:#  if AGAT name is in text:
-            return True
-        
-        print("AGAT is not called")
-        return False
     
     def _get_speach_text(audio):
         medium = Whisper("medium")
         return medium.get_text(audio)
     
-    def listen(self, dialogue_continues: bool = False):
-        if dialogue_continues:
-            start_waiting = datetime.now()
+    def listen(self: bool = False):
 
         while True:
             try:
                 audio = self.vad.listen()
+                speach_text = self._get_speach_text(audio)
+                print("text:", speach_text)
 
-                if dialogue_continues:
-                    delta_time = datetime.now() - start_waiting 
-                    if delta_time.total_seconds() > 300:
-                        dialogue_continues = False
-
-                if dialogue_continues or self._is_AGAT_called(audio):
-                    speach_text = self._get_speach_text(audio)
-                    print("text:", speach_text)
-
-                    if speach_text.text != "":
-                        return speach_text
-                    
-                    print("nothing found")
+                if speach_text.text != "":
+                    return speach_text
+                
+                print("nothing found")
             except TypeError:
                 continue
                 
---
+++ utils/data_processing/__init__.py
@@ -0,0 +1,4 @@
+from .text_prep import split_sentences, word_tokenize, make_ngrams, preprocess, seq_to_ids, ids_to_seq, lookup_token, lookup_token_id, encode, decode, make_padding_mask
+from .vocabulary import count_tokens, build_vocab
+
+from .constants import SPEC_TOKENS
